f416e166c24a7b4f6b20d4e5bd0b5f1a2a4f7489
Mark <mazhenjin@sina.com>
2022-11-04 11:27:28 +0800

    refactor Permission check in Splash page

--- app/src/main/java/org/mediasoup/droid/demo/SplashScreenActivity.java
+++ app/src/main/java/org/mediasoup/droid/demo/SplashScreenActivity.java
@@ -1,23 +1,51 @@
 package org.mediasoup.droid.demo;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_splash_screen);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        checkPermission();
+    }
 
-    findViewById(R.id.mediasoup).postDelayed(new Runnable() {
-      @Override
-      public void run() {
-        startActivity(new Intent(SplashScreenActivity.this, RoomActivity.class));
-        finish();
-      }
-    }, 1500);
-  }
+    private void enterMain() {
+        findViewById(R.id.mediasoup).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, RoomActivity.class));
+                finish();
+            }
+        }, 1500);
+    }
+
+    private PermissionHandler permissionHandler =
+            new PermissionHandler() {
+                @Override
+                public void onGranted() {
+                    enterMain();
+                }
+            };
+
+    private void checkPermission() {
+        String[] permissions = {
+                Manifest.permission.INTERNET,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        String rationale = "Please provide permissions";
+        Permissions.Options options =
+                new Permissions.Options().setRationaleDialogTitle("Info").setSettingsDialogTitle("Warning");
+        Permissions.check(this, permissions, rationale, options, permissionHandler);
+    }
 }
