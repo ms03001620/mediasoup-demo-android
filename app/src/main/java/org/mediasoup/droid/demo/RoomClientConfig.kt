cb7171d5b392b57992484684560d2aef6e3c4afe
Mark <mazhenjin@sina.com>
2022-11-04 09:54:03 +0800

    add kotlin support

---
+++ app/src/main/java/org/mediasoup/droid/demo/RoomClientConfig.kt
@@ -0,0 +1,12 @@
+package org.mediasoup.droid.demo
+
+import android.content.Context
+import android.preference.PreferenceManager
+
+class RoomClientConfig {
+
+    fun loadFromShare(context: Context) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    }
+}
