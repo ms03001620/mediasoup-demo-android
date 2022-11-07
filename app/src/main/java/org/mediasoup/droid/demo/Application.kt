da2cd0b5f935627c4224284844eb72286aa738cc
Mark <mazhenjin@sina.com>
2022-11-07 09:59:25 +0800

    Refactor kotlin Application.kt

--- app/src/main/java/org/mediasoup/droid/demo/Application.kt
+++ app/src/main/java/org/mediasoup/droid/demo/Application.kt
@@ -1,16 +1,14 @@
-package org.mediasoup.droid.demo;
+package org.mediasoup.droid.demo
 
-import org.mediasoup.droid.Logger;
-import org.mediasoup.droid.MediasoupClient;
+import android.app.Application
+import org.mediasoup.droid.Logger
+import org.mediasoup.droid.MediasoupClient
 
-public class Application extends android.app.Application {
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-
-    Logger.setLogLevel(Logger.LogLevel.LOG_DEBUG);
-    Logger.setDefaultHandler();
-    MediasoupClient.initialize(getApplicationContext());
-  }
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Logger.setLogLevel(Logger.LogLevel.LOG_DEBUG)
+        Logger.setDefaultHandler()
+        MediasoupClient.initialize(applicationContext)
+    }
 }
